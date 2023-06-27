c89bdf0bba312968976996eac8f1ade7d366a230
Christian Köhler <ckoehler@ck99.io>
2023-06-27 20:49:12 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'bootstrap_package' => '14.0.0-14.9.99',
+             'bootstrap_package' => '13.0.0-13.9.99',
         ],
         'conflicts' => [
         ],
